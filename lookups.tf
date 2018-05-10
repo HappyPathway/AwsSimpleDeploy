db7789d4614c6f07a31fe6e726b1ea2d659ac1f4
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-10 08:46:36 -0700

    changing some config paths in consul to environments rather than servcies

--- lookups.tf
+++ lookups.tf
@@ -18,31 +18,31 @@ data "consul_keys" "aws_infra" {
 
   key {
     name    = "instance_type"
-    path    = "services/${var.env}/${var.service_name}/instance_type"
+    path    = "environments/${var.env}/${var.service_name}/instance_type"
   }
 
   key {
     name    = "key_pair"
-    path    = "services/${var.env}/${var.service_name}/key_pair"
+    path    = "environments/${var.env}/${var.service_name}/key_pair"
   }
 
   key {
     name    = "instance_user"
-    path    = "services/${var.env}/${var.service_name}/instance_user"
+    path    = "environments/${var.env}/${var.service_name}/instance_user"
   }
 
   key {
     name    = "dns_name"
-    path    = "services/${var.env}/${var.service_name}/dns_name"
+    path    = "environments/${var.env}/${var.service_name}/dns_name"
   }
 
   key {
     name    = "dns_zone"
-    path    = "services/${var.env}/${var.service_name}/dns_zone"
+    path    = "environments/${var.env}/${var.service_name}/dns_zone"
   }
 
   key {
     name    = "private_zone"
-    path    = "services/${var.env}/${var.service_name}/private_zone"
+    path    = "environments/${var.env}/${var.service_name}/private_zone"
   }
 }
