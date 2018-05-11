2b076315f93e77d668dad2d2ddf5064d47a141a4
Dave Arnold <dja+djaboxx@sonic.net>
2018-05-10 17:11:34 -0700

    Update opsconfig.tf

--- opsconfig.tf
+++ opsconfig.tf
@@ -5,7 +5,7 @@ module "ops_config" {
   service_version = "${var.service_version}"
   consul_address = "${var.consul_address}"
   vault_address = "${var.vault_address}"
-  hosts = "${aws_instance.web.instance_ips}"
-  triggers = "${aws_instance.web.instance_ids}"
+  hosts = "${aws_instance.web.*.public_ip]"
+  triggers = "${aws_instance.web.*.id}"
   env = "${var.env}"
 }
